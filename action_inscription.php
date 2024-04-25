384f5047d2a13807e941961c5f608dea87d3da1a
Kenneth SOARES <kensoares@gmail.com>
2024-04-25 13:57:09 +0200

    peut pas save 2 fois le meme email

--- action_inscription.php
+++ action_inscription.php
@@ -3,13 +3,19 @@
 	include("config.php");
 		
 	if($_POST["password"] == $_POST["password_verif"]) {
-		$req = $bdd->prepare("INSERT INTO utilisateur (nom,prenom,email,mdp,credits) VALUES (?,?,?,?,0);");
-		$req->execute([$_POST["nom"],$_POST["prenom"], $_POST["email"], $_POST["password"]]);
 		
-		$bdd->query("INSERT INTO roles (email, est_prof, est_admin) VALUES ('".$_POST["email"]."', FALSE, FALSE)");			
+		$result = $bdd->query("SELECT * FROM utilisateur WHERE email='".$_POST["email"]."';");
+        $row = $result->fetch();
 
-		
-		echo 'Inscription réussie! <a href="form_connexion.php">retour à la connexion</a>';
+        if(isset($row)) {
+            echo 'email déjà utilisé, veuillez en utiliser un autre <br><a href="form_inscription.php">retour aux inscription</a>';
+        } else {
+            $req = $bdd->prepare("INSERT INTO utilisateur (nom,prenom,email,mdp,credits) VALUES (?,?,?,?,0);");
+            $req->execute([$_POST["nom"],$_POST["prenom"], $_POST["email"], $_POST["password"]]);
+            $bdd->query("INSERT INTO roles (email, est_prof, est_admin) VALUES ('".$_POST["email"]."', FALSE, FALSE)");			
+            echo 'Inscription réussie! <a href="form_connexion.php">retour à la connexion</a>';
+        }
+        
 	} else {
 		echo "Les mots de passes ne sont pas identiques.";
 		echo "<a href='form_inscription.php'>retour à l'inscription</a>";
